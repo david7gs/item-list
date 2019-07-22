d5d1c1c4be42421a182deaf476d7f5a98b344d1e
David Garcia-Shelton <dgarciashelton@gmail.com>
2019-07-21 22:58:07 -0700

    adding state - TODO checkboxes and save button

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 
 const contentNode = document.getElementById('main');
 
-const items = [
-  {id: 1, checked: true, detail: 'CheckBox1' },
-  {id: 2, checked: true, detail: 'CheckBox2' },
-  {id: 3, checked: true, detail: 'CheckBox3' },
-  {id: 4, checked: true, detail: 'CheckBox4' },
-  {id: 5, checked: false, detail: 'CheckBox5' },
+let items = [
+    {id: 1, checked: true, detail: 'CheckBox1' },
+    {id: 2, checked: true, detail: 'CheckBox2' },
+    {id: 3, checked: true, detail: 'CheckBox3' },
+    {id: 4, checked: true, detail: 'CheckBox4' },
+    {id: 5, checked: false, detail: 'CheckBox5' },
 ];
 
 class ListItem extends React.Component {
@@ -14,7 +14,7 @@ class ListItem extends React.Component {
     const item = this.props.item;
     return (
       <div className="itemDetail">
-        {item.detail}
+        { item.checked ? item.detail : '' }
       </div>
     );
   }
@@ -24,27 +24,37 @@ class ItemList extends React.Component {
   render() {
     const listItems = this.props.items.map(item => <ListItem key={item.id} item={item} />);
     return (
-      <div>{listItems}</div>
-    );
-  }
-}
-
-class Edit extends React.Component {
-  render() {
-    return (
-      <div className="editButton">
-        <button>edit</button>
+      <div>
+        {listItems}
       </div>
     );
   }
 }
 
 class EditListItem extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      items: items,
+    };
+    this.countChecks = this.countChecks.bind(this);
+  }
+
+  countChecks() {
+    let arrLength = items.length;
+    console.log('items', arrLength);
+
+    let checks = this.state.items.filter(checks => checks.checked);
+    console.log('checks =', checks.length);
+
+  };
+
   render() {
     const eItem = this.props.eItem;
     return (
-      <div className="itemDetail">
-        <input type="checkbox" defaultChecked={eItem.checked}/>
+
+      <div className="editItemList">
+        <input type="checkbox" onClick={this.countChecks} defaultChecked={eItem.checked}/>
         {eItem.detail}
       </div>
     );
@@ -53,13 +63,11 @@ class EditListItem extends React.Component {
 
 
 class EditList extends React.Component {
+
   render() {
     const editListItems = this.props.items.map(eItem => <EditListItem key={eItem.id} eItem={eItem} />);
     return (
       <div className="editListItems">
-        <button>Save It</button>
-        <button>Clear</button>
-        <button>Cancel</button>
         <div>{editListItems}</div>
       </div>
     );
@@ -67,16 +75,64 @@ class EditList extends React.Component {
 }
 
 class ItemListApp extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      items: items,
+      isEditable: false
+    };
+    this.handleEdit = this.handleEdit.bind(this);
+    this.handleSave = this.handleSave.bind(this);
+    this.handleClear = this.handleClear.bind(this);
+    this.handleCancel = this.handleCancel.bind(this);
+  }
+
+  handleEdit() {
+    this.setState({ isEditable: true });
+  };
+
+  handleSave() {
+    this.setState({ isEditable: false });
+  };
+
+  handleClear() {
+    items.forEach(item => item.checked = false);
+    this.setState({
+      isEditable: false,
+      items: items
+    });
+  }
+
+  handleCancel() {
+    this.setState({ isEditable: false });
+  };
+
+  handleChange(e) {
+    if(this.state.checked !== e.target.checked) {
+      this.setState({
+        checked:e.target.checked
+      });
+    }
+  }
+
   render() {
+    const isEditable = this.state.isEditable;
+
     return (
       <div>
         <ItemList items={items}/>
-        <Edit/>
-        <EditList items={items}/>
+        { !isEditable
+          ? <button className="editButton" onClick={this.handleEdit}>edit</button>
+          : <div className="editListItems">
+            <button onClick={this.handleSave}>Save</button>
+            <button onClick={this.handleClear} onChange={this.handleChange}>Clear</button>
+            <button onClick={this.handleCancel}>Cancel</button>
+          </div>
+        }
+        { isEditable && <EditList items={this.state.items} /> }
       </div>
     );
   }
 }
-
 
 ReactDOM.render(<ItemListApp />, contentNode);
--- static/App.js
+++ static/App.js
@@ -1,96 +1,231 @@
+'use strict';
 
-const contentNode = document.getElementById('main');
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-const items = [{ id: 1, checked: true, detail: 'CheckBox1' }, { id: 2, checked: true, detail: 'CheckBox2' }, { id: 3, checked: true, detail: 'CheckBox3' }, { id: 4, checked: true, detail: 'CheckBox4' }, { id: 5, checked: false, detail: 'CheckBox5' }];
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-class ListItem extends React.Component {
-  render() {
-    const item = this.props.item;
-    return React.createElement(
-      'div',
-      { className: 'itemDetail' },
-      item.detail
-    );
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var contentNode = document.getElementById('main');
+
+var items = [{ id: 1, checked: true, detail: 'CheckBox1' }, { id: 2, checked: true, detail: 'CheckBox2' }, { id: 3, checked: true, detail: 'CheckBox3' }, { id: 4, checked: true, detail: 'CheckBox4' }, { id: 5, checked: false, detail: 'CheckBox5' }];
+
+var ListItem = function (_React$Component) {
+  _inherits(ListItem, _React$Component);
+
+  function ListItem() {
+    _classCallCheck(this, ListItem);
+
+    return _possibleConstructorReturn(this, (ListItem.__proto__ || Object.getPrototypeOf(ListItem)).apply(this, arguments));
   }
-}
 
-class ItemList extends React.Component {
-  render() {
-    const listItems = this.props.items.map(item => React.createElement(ListItem, { key: item.id, item: item }));
-    return React.createElement(
-      'div',
-      null,
-      listItems
-    );
+  _createClass(ListItem, [{
+    key: 'render',
+    value: function render() {
+      var item = this.props.item;
+      return React.createElement(
+        'div',
+        { className: 'itemDetail' },
+        item.checked ? item.detail : ''
+      );
+    }
+  }]);
+
+  return ListItem;
+}(React.Component);
+
+var ItemList = function (_React$Component2) {
+  _inherits(ItemList, _React$Component2);
+
+  function ItemList() {
+    _classCallCheck(this, ItemList);
+
+    return _possibleConstructorReturn(this, (ItemList.__proto__ || Object.getPrototypeOf(ItemList)).apply(this, arguments));
   }
-}
 
-class Edit extends React.Component {
-  render() {
-    return React.createElement(
-      'div',
-      { className: 'editButton' },
-      React.createElement(
-        'button',
-        null,
-        'edit'
-      )
-    );
-  }
-}
-
-class EditListItem extends React.Component {
-  render() {
-    const eItem = this.props.eItem;
-    return React.createElement(
-      'div',
-      { className: 'itemDetail' },
-      React.createElement('input', { type: 'checkbox', defaultChecked: eItem.checked }),
-      eItem.detail
-    );
-  }
-}
-
-class EditList extends React.Component {
-  render() {
-    const editListItems = this.props.items.map(eItem => React.createElement(EditListItem, { key: eItem.id, eItem: eItem }));
-    return React.createElement(
-      'div',
-      { className: 'editListItems' },
-      React.createElement(
-        'button',
-        null,
-        'Save'
-      ),
-      React.createElement(
-        'button',
-        null,
-        'Clear'
-      ),
-      React.createElement(
-        'button',
-        null,
-        'Cancel'
-      ),
-      React.createElement(
+  _createClass(ItemList, [{
+    key: 'render',
+    value: function render() {
+      var listItems = this.props.items.map(function (item) {
+        return React.createElement(ListItem, { key: item.id, item: item });
+      });
+      return React.createElement(
         'div',
         null,
-        editListItems
-      )
-    );
-  }
-}
+        listItems
+      );
+    }
+  }]);
 
-class ItemListApp extends React.Component {
-  render() {
-    return React.createElement(
-      'div',
-      null,
-      React.createElement(ItemList, { items: items }),
-      React.createElement(Edit, null),
-      React.createElement(EditList, { items: items })
-    );
+  return ItemList;
+}(React.Component);
+
+var EditListItem = function (_React$Component3) {
+  _inherits(EditListItem, _React$Component3);
+
+  function EditListItem() {
+    _classCallCheck(this, EditListItem);
+
+    var _this3 = _possibleConstructorReturn(this, (EditListItem.__proto__ || Object.getPrototypeOf(EditListItem)).call(this));
+
+    _this3.state = {
+      items: items
+    };
+    _this3.countChecks = _this3.countChecks.bind(_this3);
+    return _this3;
   }
-}
+
+  _createClass(EditListItem, [{
+    key: 'countChecks',
+    value: function countChecks() {
+      var arrLength = items.length;
+      console.log('items', arrLength);
+
+      var checks = this.state.items.filter(function (checks) {
+        return checks.checked;
+      });
+      console.log('checks =', checks.length);
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      var eItem = this.props.eItem;
+      return React.createElement(
+        'div',
+        { className: 'editItemList' },
+        React.createElement('input', { type: 'checkbox', onClick: this.countChecks, defaultChecked: eItem.checked }),
+        eItem.detail
+      );
+    }
+  }]);
+
+  return EditListItem;
+}(React.Component);
+
+var EditList = function (_React$Component4) {
+  _inherits(EditList, _React$Component4);
+
+  function EditList() {
+    _classCallCheck(this, EditList);
+
+    return _possibleConstructorReturn(this, (EditList.__proto__ || Object.getPrototypeOf(EditList)).apply(this, arguments));
+  }
+
+  _createClass(EditList, [{
+    key: 'render',
+    value: function render() {
+      var editListItems = this.props.items.map(function (eItem) {
+        return React.createElement(EditListItem, { key: eItem.id, eItem: eItem });
+      });
+      return React.createElement(
+        'div',
+        { className: 'editListItems' },
+        React.createElement(
+          'div',
+          null,
+          editListItems
+        )
+      );
+    }
+  }]);
+
+  return EditList;
+}(React.Component);
+
+var ItemListApp = function (_React$Component5) {
+  _inherits(ItemListApp, _React$Component5);
+
+  function ItemListApp() {
+    _classCallCheck(this, ItemListApp);
+
+    var _this5 = _possibleConstructorReturn(this, (ItemListApp.__proto__ || Object.getPrototypeOf(ItemListApp)).call(this));
+
+    _this5.state = {
+      items: items,
+      isEditable: false
+    };
+    _this5.handleEdit = _this5.handleEdit.bind(_this5);
+    _this5.handleSave = _this5.handleSave.bind(_this5);
+    _this5.handleClear = _this5.handleClear.bind(_this5);
+    _this5.handleCancel = _this5.handleCancel.bind(_this5);
+    return _this5;
+  }
+
+  _createClass(ItemListApp, [{
+    key: 'handleEdit',
+    value: function handleEdit() {
+      this.setState({ isEditable: true });
+    }
+  }, {
+    key: 'handleSave',
+    value: function handleSave() {
+      this.setState({ isEditable: false });
+    }
+  }, {
+    key: 'handleClear',
+    value: function handleClear() {
+      items.forEach(function (item) {
+        return item.checked = false;
+      });
+      this.setState({
+        isEditable: false,
+        items: items
+      });
+    }
+  }, {
+    key: 'handleCancel',
+    value: function handleCancel() {
+      this.setState({ isEditable: false });
+    }
+  }, {
+    key: 'handleChange',
+    value: function handleChange(e) {
+      if (this.state.checked !== e.target.checked) {
+        this.setState({
+          checked: e.target.checked
+        });
+      }
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      var isEditable = this.state.isEditable;
+
+      return React.createElement(
+        'div',
+        null,
+        React.createElement(ItemList, { items: items }),
+        !isEditable ? React.createElement(
+          'button',
+          { className: 'editButton', onClick: this.handleEdit },
+          'edit'
+        ) : React.createElement(
+          'div',
+          { className: 'editListItems' },
+          React.createElement(
+            'button',
+            { onClick: this.handleSave },
+            'Save'
+          ),
+          React.createElement(
+            'button',
+            { onClick: this.handleClear, onChange: this.handleChange },
+            'Clear'
+          ),
+          React.createElement(
+            'button',
+            { onClick: this.handleCancel },
+            'Cancel'
+          )
+        ),
+        isEditable && React.createElement(EditList, { items: this.state.items })
+      );
+    }
+  }]);
+
+  return ItemListApp;
+}(React.Component);
 
 ReactDOM.render(React.createElement(ItemListApp, null), contentNode);
